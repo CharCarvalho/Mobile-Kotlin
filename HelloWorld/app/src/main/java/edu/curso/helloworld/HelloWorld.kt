a02a12f22eda56c1bf0e0a39cc3b277ccb3a1586
CharCarvalho <rm550113@fiap.com.br>
2024-08-08 00:37:29 -0300

    Commitando mudanças passadas em aula

---
+++ HelloWorld/app/src/main/java/edu/curso/helloworld/HelloWorld.kt
@@ -0,0 +1,26 @@
+package edu.curso.helloworld
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+class HelloWorld : Activity() {
+    override fun onCreate(bundle: Bundle?){
+        super.onCreate(bundle)
+        Log.i("HelloWorldApp", "onCreate Ativado")
+        val txt = TextView( this )
+        txt.text = "Hello World"
+        setContentView( txt )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("HelloWorldApp", "onStart Executado")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("HelloWorldApp", "onResume Executado")
+    }
+}
